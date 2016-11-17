bc38128599410dbe5846f869fab7fc05eb61fb73
江贵铸 <jiangguizhu@mac-book-air.local>
2016-11-17 21:38:00 +0800

    初始化项目

---
+++ DouYuZhiBo-OC/DouYuZhiBo-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DouYuZhiBo-OC
+//
+//  Created by 江贵铸 on 2016/11/17.
+//  Copyright © 2016年 江贵铸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DouYuZhiBo-OC/DouYuZhiBo-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DouYuZhiBo-OC
+//
+//  Created by 江贵铸 on 2016/11/17.
+//  Copyright © 2016年 江贵铸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
